883258493e3d14198d96ceaf44edfeedd5be423f
Saoa35 <saoamah@gmail.com>
2022-05-29 20:05:10 +0200

    + MyContext Provider

--- src/index.js
+++ src/index.js
@@ -2,11 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './Components/App/App';
+import { MyContext } from './context-api/myContext';
+
+
+const value = {
+      name: 'Alex',
+      age: 25
+  }
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <MyContext.Provider value={value}>
+      <App />
+    </MyContext.Provider>
   </React.StrictMode>
 );
