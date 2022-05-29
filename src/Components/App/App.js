1f635039844f65454d73aa471a4f34a0ff8d17cf
Saoa35 <saoamah@gmail.com>
2022-05-29 13:57:59 +0200

    + RefsExample comp

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,9 +1,10 @@
+import RefsExample from '../RefsExample/RefsExample';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <RefsExample/>
     </div>
   );
 }
